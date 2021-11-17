b8cf9f7b274857721a759699fdc139825d3ce70c
Raúl Kripalani <raul@protocol.ai>
2021-11-17 16:54:04 +0000

    move examples out of inner trees; add top-level Makefile.

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
 RUSTFLAGS="-C target-feature=+crt-static"
 
+all:
+
+.PHONY: all
+
 build:
 	cargo build
+.PHONY: build
+
+example-actor:
+	$(MAKE) -C ./examples/actor build
+.PHONY: example-actor
+
+example-fvm: example-actor
+	$(MAKE) -C ./examples/fvm build
+.PHONY: example-fvm
+
+examples: example-actor example-fvm
--- examples/fvm/src/main.rs
+++ examples/fvm/src/main.rs
@@ -2,7 +2,7 @@ use fvm;
 use wasmer::{Instance, Module, Store};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let module_wasm = include_bytes!("../../../sdk/example/fvm_example_actor.wasm");
+    let module_wasm = include_bytes!("../fvm_example_actor.wasm");
     let store = Store::default();
     let module = Module::new(&store, module_wasm)?;
     let import_object = fvm::environment(&store);
