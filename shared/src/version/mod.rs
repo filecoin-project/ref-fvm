c141c885a626b685f6483298dcdf58708f7cc2e2
Raúl Kripalani <raul@protocol.ai>
2021-11-28 16:15:42 +0000

    create a `shared` module containing high-level types and functions.
    Extracted from various modules in the Forest implementation.
    
    Needs to be trimmed down significantly to the bare essentials
    for SDK <> FVM communication and syscall handling.

---
+++ shared/src/version/mod.rs
@@ -0,0 +1,38 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use crate::encoding::repr::Serialize_repr;
+
+/// Specifies the network version
+#[derive(Debug, PartialEq, Clone, Copy, PartialOrd, Serialize_repr)]
+#[repr(u32)]
+pub enum NetworkVersion {
+    /// genesis (specs-actors v0.9.3)
+    V0,
+    /// breeze (specs-actors v0.9.7)
+    V1,
+    /// smoke (specs-actors v0.9.8)
+    V2,
+    /// ignition (specs-actors v0.9.11)
+    V3,
+    /// actors v2 (specs-actors v2.0.x)
+    V4,
+    /// tape (increases max prove commit size by 10x)
+    V5,
+    /// kumquat (specs-actors v2.2.0)
+    V6,
+    /// calico (specs-actors v2.3.2)
+    V7,
+    /// persian (post-2.3.2 behaviour transition)
+    V8,
+    /// orange
+    V9,
+    /// actors v3 (specs-actors v3.0.x)
+    V10,
+    /// norwegian (specs-actor v3.1.x)
+    V11,
+    /// actors v3 (specs-actor v4.0.x)
+    V12,
+    /// reserved
+    V13,
+}
