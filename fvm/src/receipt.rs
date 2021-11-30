0e8df1b7145384d8ffe28ef7e85ff81714ffc471
Raúl Kripalani <raul@protocol.ai>
2021-11-30 19:38:28 +0000

    add basic Receipt type.
    Taken from Forest's MessageReceipt and stripped down
    to bare essentials.

---
+++ fvm/src/receipt.rs
@@ -0,0 +1,14 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use crate::message::RawBytes;
+use encoding::tuple::*;
+use vm::{ExitCode, Serialized};
+
+/// Result of a state transition from a message
+#[derive(Debug, PartialEq, Clone, Serialize_tuple, Deserialize_tuple)]
+pub struct Receipt {
+    pub exit_code: ExitCode,
+    pub return_data: RawBytes,
+    pub gas_used: i64,
+}
