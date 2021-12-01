3c6b1a43f56f054824f31715a2509abf42b13b9c
Claudia Richoux <c@laudiacay.cool>
2021-12-01 01:32:40 -0500

    incorporating changes from forest on f12

--- utils/bitfield/src/rleplus/reader.rs
+++ utils/bitfield/src/rleplus/reader.rs
@@ -89,8 +89,13 @@ impl<'a> BitReader<'a> {
             // if the most significant bit is a 0, we've
             // reached the end of the varint
             if byte & 0x80 == 0 {
-                return Ok(len);
-            }
+                if i == 0 || byte != 0 {
+                    // only the first byte can be zero in a varint
+                    return Ok(len);
+                }
+                // not minimally encoded
+                break;    
+                        }
         }
 
         Err("Invalid varint")
