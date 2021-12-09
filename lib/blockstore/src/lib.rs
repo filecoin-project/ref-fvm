59f4725275d12299583513b3d7994f995423f1e3
Steven Allen <steven@stebalien.com>
2021-12-08 21:55:20 -0800

    flesh out buffered blockstore

--- lib/blockstore/src/lib.rs
+++ lib/blockstore/src/lib.rs
@@ -1,6 +1,5 @@
 use cid::Cid;
 
-#[cfg(feature = "buffered")]
 pub mod buffered;
 mod memory;
 pub use memory::MemoryBlockstore;
@@ -11,9 +10,6 @@ pub mod cgo;
 /// An IPLD blockstore suitable for injection into the FVM.
 ///
 /// The cgo blockstore adapter implements this trait.
-///
-// TODO: be generic over size without going insane.
-// TODO: maybe have a block _reader_?
 pub trait Blockstore {
     /// The concrete error type that the implementation will throw.
     type Error: std::error::Error + Send + Sync + 'static;
@@ -22,4 +18,15 @@ pub trait Blockstore {
     fn get(&self, k: &Cid) -> Result<Option<Vec<u8>>, Self::Error>;
     fn put(&self, k: &Cid, block: &[u8]) -> Result<(), Self::Error>;
     fn delete(&self, k: &Cid) -> Result<(), Self::Error>;
+
+    fn put_many<'a, I>(&self, blocks: I) -> Result<(), Self::Error>
+    where
+        Self: Sized,
+        I: IntoIterator<Item = (Cid, &'a [u8])>,
+    {
+        for (k, b) in blocks {
+            self.put(&k, b)?;
+        }
+        Ok(())
+    }
 }
