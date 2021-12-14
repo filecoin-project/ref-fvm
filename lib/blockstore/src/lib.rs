611372f14222120382906044da067f884190ebe0
Steven Allen <steven@stebalien.com>
2021-12-14 08:14:33 -0800

    feat: implement Blockstore for & and Rc

--- lib/blockstore/src/lib.rs
+++ lib/blockstore/src/lib.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use cid::Cid;
 
 pub mod buffered;
@@ -28,5 +30,67 @@ pub trait Blockstore {
             self.put(&k, b)?;
         }
         Ok(())
+    }
+}
+
+impl<B> Blockstore for &B
+where
+    B: Blockstore,
+{
+    type Error = B::Error;
+
+    fn has(&self, k: &Cid) -> Result<bool, Self::Error> {
+        (*self).has(k)
+    }
+
+    fn get(&self, k: &Cid) -> Result<Option<Vec<u8>>, Self::Error> {
+        (*self).get(k)
+    }
+
+    fn put(&self, k: &Cid, block: &[u8]) -> Result<(), Self::Error> {
+        (*self).put(k, block)
+    }
+
+    fn delete(&self, k: &Cid) -> Result<(), Self::Error> {
+        (*self).delete(k)
+    }
+
+    fn put_many<'a, I>(&self, blocks: I) -> Result<(), Self::Error>
+    where
+        Self: Sized,
+        I: IntoIterator<Item = (Cid, &'a [u8])>,
+    {
+        (*self).put_many(blocks)
+    }
+}
+
+impl<B> Blockstore for Rc<B>
+where
+    B: Blockstore,
+{
+    type Error = B::Error;
+
+    fn has(&self, k: &Cid) -> Result<bool, Self::Error> {
+        (**self).has(k)
+    }
+
+    fn get(&self, k: &Cid) -> Result<Option<Vec<u8>>, Self::Error> {
+        (**self).get(k)
+    }
+
+    fn put(&self, k: &Cid, block: &[u8]) -> Result<(), Self::Error> {
+        (**self).put(k, block)
+    }
+
+    fn delete(&self, k: &Cid) -> Result<(), Self::Error> {
+        (**self).delete(k)
+    }
+
+    fn put_many<'a, I>(&self, blocks: I) -> Result<(), Self::Error>
+    where
+        Self: Sized,
+        I: IntoIterator<Item = (Cid, &'a [u8])>,
+    {
+        (**self).put_many(blocks)
     }
 }
