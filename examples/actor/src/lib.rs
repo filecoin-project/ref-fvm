be1511726df6319afe8acf275b5d70ffd9f6141e
Raúl Kripalani <raul@protocol.ai>
2022-01-07 18:47:23 +0000

    back out changes to example actor.

--- examples/actor/src/lib.rs
+++ examples/actor/src/lib.rs
@@ -3,13 +3,9 @@ use fvm_sdk as sdk;
 /// Invoke is the actor entry point. It takes the ID of the parameters block, and returns the ID of
 /// the return value block.
 // NOTE: for now, the params will always just be 1. But in theory, this doesn't have to be the case.
-
 #[no_mangle]
 pub fn invoke(_: u32) -> u32 {
     //let root = sdk::ipld::get_root();
-    let _value = sdk::network::base_fee().unwrap();
-    sdk::sself::root();
-    sdk::vm::abort(2, None);
-
+    let _value = sdk::network::base_fee();
     sdk::ipld::UNIT
 }
