66932530bf7a88557bd6b349b3ce704b2ffba857
Raúl Kripalani <raul@protocol.ai>
2022-01-21 16:28:15 +0000

    move the syscall-backed Blockstore into the SDK.
    This allows custom actors to use a blockstore directly with, for example,
    AMT and HAMT data structures.
    
    The ActorBlockstore delegates to the SDK blockstore, and translates errors
    to exit codes, thus preserving the previous interface.

--- actors/runtime/src/runtime/actor_blockstore.rs
+++ actors/runtime/src/runtime/actor_blockstore.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 use cid::multihash::Code;
 use cid::Cid;
 use fvm_sdk as fvm;
+use fvm_sdk::blockstore::Blockstore;
 use fvm_shared::blockstore::Block;
 
 use crate::actor_error;
@@ -14,35 +15,23 @@ pub struct ActorBlockstore;
 /// Implements a blockstore delegating to IPLD syscalls.
 impl fvm_shared::blockstore::Blockstore for ActorBlockstore {
     fn get(&self, cid: &Cid) -> Result<Option<Vec<u8>>> {
-        // If this fails, the _CID_ is invalid. I.e., we have a bug.
-        fvm::ipld::get(cid).map(Some).map_err(|c| {
-            actor_error!(ErrIllegalState; "get failed with {:?} on CID '{}'", c, cid).into()
-        })
+        Blockstore
+            .get(cid)
+            .map_err(|err| actor_error!(ErrIllegalState; err.to_string()).into())
     }
 
     fn put_keyed(&self, k: &Cid, block: &[u8]) -> Result<()> {
-        let code = Code::try_from(k.hash().code())
-            .map_err(|e| actor_error!(ErrSerialization, e.to_string()))?;
-        let k2 = self.put(code, &Block::new(k.codec(), block))?;
-        if k != &k2 {
-            Err(
-                actor_error!(ErrSerialization; "put block with cid {} but has cid {}", k, k2)
-                    .into(),
-            )
-        } else {
-            Ok(())
-        }
+        Blockstore
+            .put_keyed(k, block)
+            .map_err(|err| actor_error!(ErrSerialization; err.to_string()).into())
     }
 
     fn put<D>(&self, code: Code, block: &Block<D>) -> Result<Cid>
     where
         D: AsRef<[u8]>,
     {
-        // TODO: Don't hard-code the size. Unfortunately, there's no good way to get it from the
-        //  codec at the moment.
-        const SIZE: u32 = 32;
-        let k = fvm::ipld::put(code.into(), SIZE, block.codec, block.data.as_ref())
-            .map_err(|c| actor_error!(ErrIllegalState; "put failed with {:?}", c))?;
-        Ok(k)
+        Blockstore
+            .put(code, block)
+            .map_err(|err| actor_error!(ErrIllegalState; err.to_string()).into())
     }
 }
--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod actor;
+pub mod blockstore;
 pub mod crypto;
 pub mod debug;
 pub mod gas;
