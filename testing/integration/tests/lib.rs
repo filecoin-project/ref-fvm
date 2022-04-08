16c4a8a27b4dff6353a74de2600ea3f1a9b16845
tchataigner <tom.chataigner@yahoo.fr>
2022-04-08 13:02:30 +0200

    :construction: Should implement testing

--- testing/integration/tests/lib.rs
+++ testing/integration/tests/lib.rs
@@ -1,11 +1,1 @@
-use fvm_integration_tests;
-
-#[test]
-pub fn test_metadata() {
-    let setup = fvm_integration_tests::setup().unwrap();
-    let mut exec = fvm_integration_tests::create_executor(
-        setup.state_root,
-        setup.builtin_actors,
-        setup.blockstore,
-    );
-}
+// TODO implement relevant testing
