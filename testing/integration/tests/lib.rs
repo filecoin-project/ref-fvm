4f4cd7ed87034844a7d8822575d6b31e13f8d339
Łukasz Magiera <magik6k@gmail.com>
2022-04-27 18:53:38 +0200

    gas integration test

--- testing/integration/tests/lib.rs
+++ testing/integration/tests/lib.rs
@@ -9,6 +9,8 @@ use fvm_shared::message::Message;
 use fvm_shared::state::StateTreeVersion;
 use fvm_shared::version::NetworkVersion;
 use num_traits::Zero;
+use wabt::wat2wasm;
+use fvm_shared::error::ExitCode;
 
 /// The state object.
 #[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
@@ -64,4 +66,59 @@ fn hello_world() {
         .unwrap();
 
     assert_eq!(res.msg_receipt.exit_code.value(), 16)
+}
+
+
+#[test]
+fn out_of_gas() {
+    const WAT: &str = r#"
+    ;; Mock invoke function
+    (module
+      (func (export "invoke") (param $x i32) (result i32)
+        (loop
+            (br 0)
+        )
+        (i32.const 1)
+      )
+    )
+    "#;
+
+    // Instantiate tester
+    let mut tester = Tester::new(NetworkVersion::V16, StateTreeVersion::V4).unwrap();
+
+    let sender: [Account; 1] = tester.create_accounts().unwrap();
+
+    // Get wasm bin
+    let wasm_bin = wat2wasm(WAT).unwrap();
+
+    // Set actor state
+    let actor_state = State { count: 0 };
+    let state_cid = tester.set_state(&actor_state).unwrap();
+
+    // Set actor
+    let actor_address = Address::new_id(10000);
+
+    tester
+        .set_actor_from_bin(&wasm_bin, state_cid, actor_address, BigInt::zero())
+        .unwrap();
+
+    // Instantiate machine
+    tester.instantiate_machine().unwrap();
+
+    // Send message
+    let message = Message {
+        from: sender[0].1,
+        to: actor_address,
+        gas_limit: 10_000_000,
+        method_num: 1,
+        ..Message::default()
+    };
+
+    let res = tester
+        .executor
+        .unwrap()
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    assert_eq!(res.msg_receipt.exit_code, ExitCode::SYS_OUT_OF_GAS)
 }
