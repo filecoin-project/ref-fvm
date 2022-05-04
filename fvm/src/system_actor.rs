21717136718a77b07d41e48a0c06c980fef5def0
vyzo <vyzo@hackzen.org>
2022-05-04 15:03:02 +0300

    use tuple serialization for system actor state

--- fvm/src/system_actor.rs
+++ fvm/src/system_actor.rs
@@ -1,16 +1,16 @@
 use anyhow::Context;
 use cid::Cid;
 use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::tuple::{Deserialize_tuple, Serialize_tuple};
 use fvm_ipld_encoding::{Cbor, CborStore};
 use fvm_shared::address::Address;
-use serde::{Deserialize, Serialize};
 
 use crate::kernel::{ClassifyResult, Result};
 use crate::state_tree::{ActorState, StateTree};
 
 pub const SYSTEM_ACTOR_ADDR: Address = Address::new_id(0);
 
-#[derive(Default, Deserialize, Serialize)]
+#[derive(Default, Deserialize_tuple, Serialize_tuple)]
 pub struct State {
     // builtin actor registry: Vec<(String, Cid)>
     pub builtin_actors: Cid,
