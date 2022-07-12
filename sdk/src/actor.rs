8b6fd6936dbc813481ff7d4258b4df23760c24bc
vyzo <vyzo@hackzen.org>
2022-07-12 18:32:12 +0300

    add comment about priviledge nature of install_actor in sdk

--- sdk/src/actor.rs
+++ sdk/src/actor.rs
@@ -63,6 +63,7 @@ pub fn create_actor(actor_id: ActorID, code_cid: &Cid) -> SyscallResult<()> {
 }
 
 /// Installs or ensures an actor code CID is valid and loaded.
+/// Note: this is a priviledged syscall, restricted to the init actor.
 #[cfg(feature = "m2-native")]
 pub fn install_actor(code_cid: &Cid) -> SyscallResult<()> {
     let cid = code_cid.to_bytes();
