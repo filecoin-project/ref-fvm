15f2b6db3eaf766ebccbffb46b68c684ebc0c20f
mriise <mark.riise26@gmail.com>
2022-08-12 12:48:50 -0700

    rustfmt (clippy is also doing weird things but it seems to be a local issue maybe)

--- testing/integration/tests/fil-syscall-actor/src/lib.rs
+++ testing/integration/tests/fil-syscall-actor/src/lib.rs
@@ -1,7 +1,6 @@
-use fvm_ipld_encoding::{to_vec, BytesSer, DAG_CBOR};
 use fvm_sdk as sdk;
-use fvm_shared::error::ExitCode;
 use fvm_shared::crypto::hash::SupportedHashes;
+use fvm_shared::error::ExitCode;
 
 #[no_mangle]
 pub fn invoke(_: u32) -> u32 {
@@ -15,22 +14,22 @@ pub fn invoke(_: u32) -> u32 {
     test_expected_hash();
     test_hash_syscall();
 
-
     #[cfg(coverage)]
     sdk::debug::store_artifact("syscall_actor.profraw", minicov::capture_coverage());
     0
 }
 
-// use SDK methods to hash and compares against locally (inside the actor) hashed digest 
-fn test_expected_hash() {    
+// use SDK methods to hash and compares against locally (inside the actor) hashed digest
+fn test_expected_hash() {
     let test_bytes = b"foo bar baz boxy";
+    
 
     let blake_local = SupportedHashes::Blake2b256.digest(test_bytes).digest();
     let blake_arr = sdk::crypto::hash_blake2b(test_bytes); // test against old SDK method since it does less unsafe things
     let blake_vec = sdk::crypto::hash(SupportedHashes::Blake2b256, test_bytes);
 
     assert_eq!(blake_arr.as_slice(), blake_vec.as_slice());
-    assert_eq!(blake_local.as_slice(), blake_vec.as_slice());
+    assert_eq!(blake_local, blake_vec.as_slice());
 
     expected_digest!(SupportedHashes::Sha2_256, test_bytes);
     expected_digest!(SupportedHashes::Keccak256, test_bytes);
@@ -38,18 +37,16 @@ fn test_expected_hash() {
 }
 
 macro_rules! expected_digest {
-    ($hasher:expr, $data:expr) => {
-        { // scoped so we can re-use variable names
-            let local_digest = $hasher.digest($data).digest();
-            let digest = sdk::crypto::hash($hasher, $data);
+    ($hasher:expr, $data:expr) => {{
+        // scoped so we can re-use variable names
+        let local_digest = $hasher.digest($data).digest();
+        let digest = sdk::crypto::hash($hasher, $data);
 
-            assert_eq!(local_digest.as_slice(), digest.as_slice());
-        }
-    };
+        assert_eq!(local_digest, digest.as_slice());
+    }};
 }
 
 // do funky things with hash syscall directly
 fn test_hash_syscall() {
     // TODO
-
 }
