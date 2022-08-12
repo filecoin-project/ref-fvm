a542a8178c0b963003d4161b1c978c99188ab5b3
mriise <mark.riise26@gmail.com>
2022-08-12 15:34:54 -0700

    remove test generating macro cuz it doesnt work for whatever reason

--- testing/integration/tests/fil-syscall-actor/src/lib.rs
+++ testing/integration/tests/fil-syscall-actor/src/lib.rs
@@ -2,6 +2,8 @@ use fvm_sdk as sdk;
 use fvm_shared::crypto::hash::SupportedHashes;
 use fvm_shared::error::ExitCode;
 
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+
 #[no_mangle]
 pub fn invoke(_: u32) -> u32 {
     std::panic::set_hook(Box::new(|info| {
@@ -21,29 +23,39 @@ pub fn invoke(_: u32) -> u32 {
 
 // use SDK methods to hash and compares against locally (inside the actor) hashed digest
 fn test_expected_hash() {
+    use multihash::MultihashDigest;
     let test_bytes = b"foo bar baz boxy";
-    
 
-    let blake_local = SupportedHashes::Blake2b256.digest(test_bytes).digest();
+    let blake_local = SupportedHashes::Blake2b256.digest(test_bytes);
     let blake_arr = sdk::crypto::hash_blake2b(test_bytes); // test against old SDK method since it does less unsafe things
     let blake_vec = sdk::crypto::hash(SupportedHashes::Blake2b256, test_bytes);
 
     assert_eq!(blake_arr.as_slice(), blake_vec.as_slice());
-    assert_eq!(blake_local, blake_vec.as_slice());
+    assert_eq!(blake_local.digest(), blake_vec.as_slice());
 
-    expected_digest!(SupportedHashes::Sha2_256, test_bytes);
-    expected_digest!(SupportedHashes::Keccak256, test_bytes);
-    expected_digest!(SupportedHashes::Ripemd160, test_bytes);
-}
+    // macros dont work so im stuck with writing this out manually
 
-macro_rules! expected_digest {
-    ($hasher:expr, $data:expr) => {{
-        // scoped so we can re-use variable names
-        let local_digest = $hasher.digest($data).digest();
-        let digest = sdk::crypto::hash($hasher, $data);
+    //sha
+    {
+        let local_digest = SupportedHashes::Sha2_256.digest(test_bytes);
+        let digest = sdk::crypto::hash(SupportedHashes::Sha2_256, test_bytes);
 
-        assert_eq!(local_digest, digest.as_slice());
-    }};
+        assert_eq!(local_digest.digest(), digest.as_slice());
+    }
+    // keccack
+    {
+        let local_digest = SupportedHashes::Keccak256.digest(test_bytes);
+        let digest = sdk::crypto::hash(SupportedHashes::Keccak256, test_bytes);
+
+        assert_eq!(local_digest.digest(), digest.as_slice());
+    }
+    // ripemd
+    {
+        let local_digest = SupportedHashes::Ripemd160.digest(test_bytes);
+        let digest = sdk::crypto::hash(SupportedHashes::Ripemd160, test_bytes);
+
+        assert_eq!(local_digest.digest(), digest.as_slice());
+    }
 }
 
 // do funky things with hash syscall directly
