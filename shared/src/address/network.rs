d8ef96d78c0b72a014d417fd918b40b94a0a3ff3
Steven Allen <steven@stebalien.com>
2022-10-06 14:02:28 -0700

    Revert "fix: update set_default_network to use store (#929)"
    This reverts commit a65c007c97f7166266aaa6227c2f2b60be2e46b4.

--- shared/src/address/network.rs
+++ shared/src/address/network.rs
@@ -42,8 +42,13 @@ pub fn default_network() -> Network {
 }
 
 /// Sets the default network
-pub fn set_default_network(network: Network) {
-    ATOMIC_NETWORK.store(network.to_u8().unwrap_or_default(), Ordering::Relaxed)
+pub fn set_default_network(network: Network) -> anyhow::Result<()> {
+    ATOMIC_NETWORK
+        .fetch_update(Ordering::Relaxed, Ordering::Relaxed, move |_| {
+            network.to_u8()
+        })
+        .map_err(|err| anyhow::Error::msg(format!("Failed to set default network: {err}")))?;
+    Ok(())
 }
 
 #[cfg(test)]
@@ -52,15 +57,16 @@ mod tests {
     use crate::address::Address;
 
     #[test]
-    fn set_network() {
+    fn set_network() -> anyhow::Result<()> {
         assert_eq!(default_network(), Network::default());
         // TODO: Consider using `enum_iterator::all::<Network>()`
         // which requires rust toolchain upgrade.
         for network in [Network::Mainnet, Network::Testnet] {
-            set_default_network(network);
+            set_default_network(network)?;
             assert_eq!(default_network(), network);
             let addr = Address::new_id(0);
             assert_eq!(addr.network(), network);
         }
+        Ok(())
     }
 }
