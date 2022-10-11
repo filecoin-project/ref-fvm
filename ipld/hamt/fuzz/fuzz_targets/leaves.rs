cd1fa5bfb2c1e23bfed6259c9ce192f18895a2dc
Akosh Farkash <aakoshh@gmail.com>
2022-10-11 11:39:55 +0100

    EVM-892: Fuzz test for pushing into leaves.

---
+++ ipld/hamt/fuzz/fuzz_targets/leaves.rs
@@ -0,0 +1,18 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+#![no_main]
+use fvm_ipld_hamt::Config;
+use libfuzzer_sys::fuzz_target;
+
+mod common;
+
+fuzz_target!(|data: (u8, Vec<common::Operation>)| {
+    let (flush_rate, operations) = data;
+    let conf = Config {
+        push_data_to_leaves: true,
+        bit_width: 4,
+        ..Default::default()
+    };
+    common::run(flush_rate, operations, conf);
+});
