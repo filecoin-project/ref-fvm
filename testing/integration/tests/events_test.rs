19f88c344715012da3036e63657bc16b14bdabfe
Raúl Kripalani <raul@protocol.ai>
2022-11-06 18:58:55 +0000

    events: implement basic integration tests.

---
+++ testing/integration/tests/events_test.rs
@@ -0,0 +1,90 @@
+mod bundles;
+use bundles::*;
+use fil_events_actor::WASM_BINARY as ADDRESS_BINARY;
+use fvm::executor::{ApplyKind, Executor};
+use fvm::machine::Machine;
+use fvm_integration_tests::dummy::DummyExterns;
+use fvm_ipld_amt::Amt;
+use fvm_ipld_blockstore::MemoryBlockstore;
+use fvm_shared::address::Address;
+use fvm_shared::econ::TokenAmount;
+use fvm_shared::error::ExitCode;
+use fvm_shared::event::StampedEvent;
+use fvm_shared::message::Message;
+use fvm_shared::state::StateTreeVersion;
+use fvm_shared::version::NetworkVersion;
+use num_traits::Zero;
+
+#[test]
+fn events_test() {
+    // Instantiate tester
+    let mut tester = new_tester(
+        NetworkVersion::V18,
+        StateTreeVersion::V4,
+        MemoryBlockstore::default(),
+    )
+    .unwrap();
+
+    let [(_sender_id, sender_address)] = tester.create_accounts().unwrap();
+
+    let wasm_bin = ADDRESS_BINARY.unwrap();
+
+    // Set actor state
+    let actor_state = [(); 0];
+    let state_cid = tester.set_state(&actor_state).unwrap();
+
+    // Set actor
+    let actor_address = Address::new_id(10000);
+
+    tester
+        .set_actor_from_bin(wasm_bin, state_cid, actor_address, TokenAmount::zero())
+        .unwrap();
+
+    // Instantiate machine
+    tester.instantiate_machine(DummyExterns).unwrap();
+
+    let executor = tester.executor.as_mut().unwrap();
+
+    // === Emits two events ===
+
+    let message = Message {
+        from: sender_address,
+        to: actor_address,
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 0,
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message.clone(), ApplyKind::Explicit, 100)
+        .unwrap();
+
+    assert_eq!(ExitCode::OK, res.msg_receipt.exit_code);
+
+    // Check that we got two events.
+    assert_eq!(2, res.events.len());
+
+    // Check the events AMT.
+    let events_amt: Amt<StampedEvent, _> =
+        Amt::load(&res.msg_receipt.events, executor.blockstore()).unwrap();
+    assert_eq!(2, events_amt.count());
+
+    // Check that events in the AMT match events returned in ApplyRet.
+    for (i, evt) in res.events.iter().enumerate() {
+        assert_eq!(Some(evt), events_amt.get(i as u64).unwrap());
+    }
+
+    // === Emits an improperly formatted event ===
+    let message = Message {
+        method_num: 3,
+        sequence: 1,
+        ..message
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    assert_eq!(ExitCode::OK, res.msg_receipt.exit_code);
+}
---
+++ testing/integration/tests/fil-events-actor/src/actor.rs
@@ -0,0 +1,87 @@
+use fvm_ipld_encoding::{to_vec, Cbor, RawBytes};
+use fvm_sdk as sdk;
+use fvm_sdk::vm;
+use fvm_shared::address::{Address, SECP_PUB_LEN};
+use fvm_shared::bigint::Zero;
+use fvm_shared::error::ExitCode;
+use fvm_shared::event::{Entry, Flags, StampedEvent, FLAG_INDEXED};
+use sdk::sys::ErrorNumber;
+use serde::{Deserialize, Serialize};
+use serde_tuple::*;
+
+#[derive(Serialize_tuple, Deserialize_tuple, PartialEq, Eq, Clone, Debug)]
+struct EventPayload1 {
+    a: String,
+    b: String,
+}
+
+impl Cbor for EventPayload1 {}
+
+#[derive(Serialize_tuple, Deserialize_tuple, PartialEq, Eq, Clone, Debug)]
+struct EventPayload2 {
+    c: i32,
+    d: Vec<u64>,
+}
+
+impl Cbor for EventPayload2 {}
+
+#[no_mangle]
+pub fn invoke(params: u32) -> u32 {
+    sdk::initialize();
+
+    const EmitSeveralOk: u64 = 2;
+    const EmitMalformed: u64 = 3;
+
+    // Emit a single-entry event.
+    let payload = EventPayload1 {
+        a: String::from("aaa111"),
+        b: String::from("bbb111"),
+    };
+
+    let single_entry_evt = vec![Entry {
+        flags: Flags(FLAG_INDEXED),
+        key: "foo".to_owned(),
+        value: payload.marshal_cbor().unwrap(),
+    }];
+
+    let payload1 = EventPayload1 {
+        a: String::from("aaa222"),
+        b: String::from("bbb222"),
+    };
+    let payload2 = EventPayload2 {
+        c: 42,
+        d: vec![1, 2, 3, 4],
+    };
+
+    let multi_entry = vec![
+        Entry {
+            flags: Flags(0),
+            key: "bar".to_owned(),
+            value: payload1.marshal_cbor().unwrap(),
+        },
+        Entry {
+            flags: Flags(FLAG_INDEXED),
+            key: "baz".to_string(),
+            value: payload2.marshal_cbor().unwrap(),
+        },
+    ];
+
+    match sdk::message::method_number() {
+        EmitSeveralOk => {
+            sdk::event::emit_event(single_entry_evt.into()).unwrap();
+            sdk::event::emit_event(multi_entry.into()).unwrap();
+        }
+        EmitMalformed => unsafe {
+            // mangle an event.
+            let mut serialized = single_entry_evt.marshal_cbor().unwrap();
+            serialized[1] = 0xff;
+
+            assert!(
+                sdk::sys::event::emit_event(serialized.as_ptr(), serialized.len() as u32).is_err(),
+                "expected failed syscall"
+            );
+        },
+        _ => panic!("invalid method number"),
+    }
+    0
+}
