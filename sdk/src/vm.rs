3496c2e04f6be1274f274777cd5ffb691feee941
vyzo <vyzo@hackzen.org>
2022-11-18 13:39:45 +0200

    sdk: vm::exit syscall and rewrite abort to use it

--- sdk/src/vm.rs
+++ sdk/src/vm.rs
@@ -1,5 +1,6 @@
 use std::ptr;
 
+use fvm_ipld_encoding::{RawBytes, DAG_CBOR};
 use fvm_shared::error::ExitCode;
 
 use crate::sys;
@@ -9,6 +10,11 @@ pub const NO_DATA_BLOCK_ID: u32 = 0;
 
 /// Abort execution.
 pub fn abort(code: u32, message: Option<&str>) -> ! {
+    exit(code, message, None)
+}
+
+/// Exit from current message execution, with the specified code and an optional message and data.
+pub fn exit(code: u32, message: Option<&str>, data: Option<RawBytes>) -> ! {
     unsafe {
         let (message, message_len) = if let Some(m) = message {
             (m.as_ptr(), m.len())
@@ -16,7 +22,19 @@ pub fn abort(code: u32, message: Option<&str>) -> ! {
             (ptr::null(), 0)
         };
 
-        sys::vm::abort(code, message, message_len as u32);
+        // Ideally we would write this double if as if let Some(bytes) = data && bytes.len() > 0
+        // but the compiler doexn't accept it.
+        let blk_id = if let Some(bytes) = data {
+            if bytes.len() > 0 {
+                sys::ipld::block_create(DAG_CBOR, bytes.as_ptr(), bytes.len() as u32).unwrap()
+            } else {
+                NO_DATA_BLOCK_ID
+            }
+        } else {
+            NO_DATA_BLOCK_ID
+        };
+
+        sys::vm::exit(code, message, message_len as u32, blk_id);
     }
 }
 
