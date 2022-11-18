f5c121379521ed040f9d081bfc5083c5f3d9ea05
Akosh Farkash <aakoshh@gmail.com>
2022-11-18 11:04:03 +0000

    EVM-892: More variation in fuzzing.

--- ipld/hamt/fuzz/fuzz_targets/extensions.rs
+++ ipld/hamt/fuzz/fuzz_targets/extensions.rs
@@ -7,11 +7,11 @@ use libfuzzer_sys::fuzz_target;
 
 mod common;
 
-fuzz_target!(|data: (u8, Vec<common::Operation>)| {
-    let (flush_rate, operations) = data;
+fuzz_target!(|data: (u8, u32, u32, Vec<common::Operation>)| {
+    let (flush_rate, bit_width, min_data_depth, operations) = data;
     let conf = Config {
-        bit_width: 2,
-        min_data_depth: 1,
+        bit_width: 1 + bit_width % 8,
+        min_data_depth: 0 + min_data_depth % 3,
     };
     common::run(flush_rate, operations, conf);
 });
