6d65867e4a3b43ee313fe9585c242b58558538f7
vyzo <vyzo@hackzen.org>
2022-11-28 20:59:59 +0200

    clean compile

--- src/fevm.rs
+++ src/fevm.rs
@@ -1,9 +1,8 @@
-use cid::Cid;
-use anyhow::anyhow;
+use anyhow;
 use fvm_integration_tests::tester::Tester;
 use fvm_ipld_blockstore::Blockstore;
 use fvm::externs::Externs;
 
-pub fn run<B: Blockstore, E: Externs>(tester: &mut Tester<B, E>, contract: &[u8], entrypoint: &[u8], params: &[u8]) -> anyhow::Result<()> {
+pub fn run<B: Blockstore, E: Externs>(_tester: &mut Tester<B, E>, _contract: &[u8], _entrypoint: &[u8], _params: &[u8]) -> anyhow::Result<()> {
     Ok(())
 }
