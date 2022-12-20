ace7b7545962aac5b7ceedc5d6c17deecf4c8fd3
Steven Allen <steven@stebalien.com>
2022-12-19 20:06:58 -0800

    fix build

--- testing/integration/tests/fil-events-actor/src/actor.rs
+++ testing/integration/tests/fil-events-actor/src/actor.rs
@@ -1,7 +1,6 @@
 // Copyright 2021-2023 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
 use fvm_ipld_encoding::ipld_block::IpldBlock;
-use fvm_ipld_encoding::Cbor;
 use fvm_sdk as sdk;
 use fvm_shared::address::Address;
 use fvm_shared::bigint::Zero;
