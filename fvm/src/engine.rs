b72fa3cf5e8e781db6f168d9ecf8a3ffa2477c81
Łukasz Magiera <magik6k@gmail.com>
2023-01-13 12:42:25 +0100

    re-enable backtrace to avoid wasmtime asserts failing

--- fvm/src/engine.rs
+++ fvm/src/engine.rs
@@ -171,13 +171,15 @@ fn wasmtime_config(ec: &EngineConfig) -> anyhow::Result<wasmtime::Config> {
     c.generate_address_map(false);
     c.cranelift_debug_verifier(false);
     c.native_unwind_info(false);
-    #[allow(deprecated)] // TODO https://github.com/bytecodealliance/wasmtime/issues/5037
-    c.wasm_backtrace(false);
     c.wasm_reference_types(false);
 
     // Reiterate some defaults
     c.guard_before_linear_memory(true);
     c.parallel_compilation(true);
+
+    #[allow(deprecated)] // TODO https://github.com/bytecodealliance/wasmtime/issues/5037
+    // has to be enabled, otherwise returning errors from syscalls will not work
+    c.wasm_backtrace(true);
 
     #[cfg(feature = "wasmtime/async")]
     c.async_support(false);
