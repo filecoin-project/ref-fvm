07f13499935dea2212611dc192870dd14971da55
ozwaldorf <self@ossian.dev>
2023-01-20 17:54:44 -0500

    feat: `CarReader::read_into()` (#1524)
    * feat: load_car_reader
    
    * tests: add test for loading car reader into blockstore
    
    * refactor: move `load_car_reader` -> `read_into` method on the reader itself
    
    * chore: remove unnecessary buf reader from test

--- ipld/car/tests/car_file_test.rs
+++ ipld/car/tests/car_file_test.rs
@@ -5,7 +5,7 @@
 use async_std::fs::File;
 use async_std::io::BufReader;
 use fvm_ipld_blockstore::MemoryBlockstore;
-use fvm_ipld_car::load_car;
+use fvm_ipld_car::{load_car, CarReader};
 
 #[async_std::test]
 async fn load_into_blockstore() {
@@ -14,4 +14,19 @@ async fn load_into_blockstore() {
     let bs = MemoryBlockstore::default();
 
     let _ = load_car(&bs, buf_reader).await.unwrap();
+}
+
+#[async_std::test]
+async fn load_car_reader_into_blockstore() {
+    let file = File::open("tests/test.car").await.unwrap();
+    let car_reader = CarReader::new(file).await.unwrap();
+    let bs = MemoryBlockstore::default();
+
+    // perform some action with the reader
+    let roots = car_reader.header.roots.clone();
+
+    // load it into the blockstore
+    let res = car_reader.read_into(&bs).await.unwrap();
+
+    assert_eq!(res, roots);
 }
