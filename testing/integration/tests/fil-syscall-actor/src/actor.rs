5f092991ccb32680d01c6533c78969467cd8c603
Fridrik Asmundsson <fridrik01@gmail.com>
2023-03-06 14:27:03 +0000

    feat: Add tests for sdk::crypto::recover_secp_public_key

--- testing/integration/tests/fil-syscall-actor/src/actor.rs
+++ testing/integration/tests/fil-syscall-actor/src/actor.rs
@@ -4,7 +4,7 @@ use fvm_sdk as sdk;
 use fvm_shared::address::Address;
 use fvm_shared::chainid::ChainID;
 use fvm_shared::crypto::hash::SupportedHashes as SharedSupportedHashes;
-use fvm_shared::crypto::signature::Signature;
+use fvm_shared::crypto::signature::{Signature, SECP_SIG_LEN};
 use fvm_shared::error::{ErrorNumber, ExitCode};
 use multihash::derive::Multihash;
 use multihash::{Blake2b256, Blake2b512, Keccak256, Ripemd160, Sha2_256};
@@ -34,7 +34,7 @@ pub fn invoke(_: u32) -> u32 {
         )
     }));
 
-    test_verify_signature();
+    test_signature();
     test_expected_hash();
     test_hash_syscall();
     test_network_context();
@@ -46,22 +46,22 @@ pub fn invoke(_: u32) -> u32 {
     0
 }
 
-fn test_verify_signature() {
+fn test_signature() {
     // the following vectors represent a valid secp256k1 signatures for an address and plaintext message
     //
-    let signature_bytes = vec![
+    let signature_bytes: Vec<u8> = vec![
         80, 210, 71, 248, 219, 226, 85, 142, 143, 235, 164, 155, 239, 68, 193, 23, 191, 215, 35,
         70, 25, 34, 203, 14, 116, 134, 214, 3, 91, 22, 196, 172, 105, 154, 134, 128, 228, 172, 12,
         25, 251, 166, 51, 0, 210, 45, 23, 91, 12, 18, 228, 43, 204, 157, 233, 81, 69, 3, 44, 121,
         167, 31, 168, 52, 0,
     ];
-    let pub_key_bytes = vec![
+    let pub_key_bytes: Vec<u8> = vec![
         4, 223, 38, 78, 238, 254, 121, 58, 63, 120, 109, 108, 179, 105, 76, 211, 252, 223, 226, 1,
         20, 220, 212, 77, 23, 190, 224, 138, 62, 103, 27, 48, 60, 150, 151, 233, 30, 217, 137, 151,
         208, 24, 212, 117, 32, 94, 44, 118, 125, 40, 25, 31, 67, 154, 106, 97, 110, 32, 209, 62,
         194, 146, 27, 16, 114,
     ];
-    let message = vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3];
+    let message: Vec<u8> = vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3];
 
     // test the happy path
     //
@@ -72,15 +72,15 @@ fn test_verify_signature() {
 
     // test with invalid signature
     //
-    let mut invalid_signature_bytes = signature_bytes;
+    let mut invalid_signature_bytes = signature_bytes.clone();
     invalid_signature_bytes[0] += 1;
-    let invalid_signature = Signature::new_secp256k1(invalid_signature_bytes);
+    let invalid_signature = Signature::new_secp256k1(invalid_signature_bytes.clone());
     let res = sdk::crypto::verify_signature(&invalid_signature, &address, &message.as_slice());
     assert_eq!(res, Ok(false));
 
     // test with invalid address
     //
-    let mut invalid_pub_key_bytes = pub_key_bytes;
+    let mut invalid_pub_key_bytes = pub_key_bytes.clone();
     invalid_pub_key_bytes[0] += 1;
     let invalid_address = Address::new_secp256k1(&invalid_pub_key_bytes).unwrap();
     let res = sdk::crypto::verify_signature(&signature, &invalid_address, &message.as_slice());
@@ -123,6 +123,20 @@ fn test_verify_signature() {
             message.as_ptr(),
             message.len() as u32,
         );
+        assert_eq!(res, Err(ErrorNumber::IllegalArgument));
+    }
+
+    // test we can recover the public key from the signature
+    //
+    let hash = sdk::crypto::hash_blake2b(&message);
+    let sig: [u8; SECP_SIG_LEN] = signature_bytes.clone().try_into().unwrap();
+    let res = sdk::crypto::recover_secp_public_key(&hash, &sig).unwrap();
+    assert_eq!(res, pub_key_bytes.as_slice());
+
+    // test that passing an invalid hash buffer results in IllegalArgument
+    //
+    unsafe {
+        let res = sdk::sys::crypto::recover_secp_public_key(hash.as_ptr(), (u32::MAX) as *const u8);
         assert_eq!(res, Err(ErrorNumber::IllegalArgument));
     }
 }
