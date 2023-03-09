4278483d4753135ea7f61099bce48c54e3443b38
Fridrik Asmundsson <fridrik01@gmail.com>
2023-03-09 12:02:37 +0000

    Refactor actor wasm bin constants in integration test

--- testing/integration/tests/actor_wasm_path/mod.rs
+++ testing/integration/tests/actor_wasm_path/mod.rs
@@ -2,73 +2,33 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 // constants for wasm build artifacts
 //
-#[allow(dead_code)]
-pub const READONLY_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_readonly_actor.wasm"
-));
-#[allow(dead_code)]
-pub const ADDRESS_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_address_actor.wasm"
-));
-#[allow(dead_code)]
-pub const HELLO_WORLD_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_hello_world_actor.wasm"
-));
-#[allow(dead_code)]
-pub const STACK_OVERFLOW_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_stack_overflow_actor.wasm"
-));
-#[allow(dead_code)]
-pub const IPLD_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_ipld_actor.wasm"
-));
-#[allow(dead_code)]
-pub const MALFORMED_SYSCALL_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_malformed_syscall_actor.wasm"
-));
-#[allow(dead_code)]
-pub const INTEGER_OVERFLOW_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_integer_overflow_actor.wasm"
-));
-#[allow(dead_code)]
-pub const SYSCALL_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_syscall_actor.wasm"
-));
-#[allow(dead_code)]
-pub const EVENTS_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_events_actor.wasm"
-));
-#[allow(dead_code)]
-pub const EXIT_DATA_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_exit_data_actor.wasm"
-));
-#[allow(dead_code)]
-pub const GASLIMIT_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_gaslimit_actor.wasm"
-));
-#[allow(dead_code)]
-pub const CREATE_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_create_actor.wasm"
-));
-#[allow(dead_code)]
-pub const OOM_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_oom_actor.wasm"
-));
-#[allow(dead_code)]
-pub const SSELF_ACTOR_BINARY: &[u8] = include_bytes!(concat!(
-    env!("OUT_DIR"),
-    "/bundle/wasm32-unknown-unknown/wasm/fil_sself_actor.wasm"
-));
+#![allow(dead_code)]
+
+macro_rules! wasm_bin {
+    ($x: expr) => {
+        concat!(
+            env!("OUT_DIR"),
+            "/bundle/wasm32-unknown-unknown/wasm/",
+            $x,
+            ".wasm"
+        )
+    };
+}
+
+pub const READONLY_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_readonly_actor"));
+pub const ADDRESS_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_address_actor"));
+pub const HELLO_WORLD_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_hello_world_actor"));
+pub const STACK_OVERFLOW_ACTOR_BINARY: &[u8] =
+    include_bytes!(wasm_bin!("fil_stack_overflow_actor"));
+pub const IPLD_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_ipld_actor"));
+pub const MALFORMED_SYSCALL_ACTOR_BINARY: &[u8] =
+    include_bytes!(wasm_bin!("fil_malformed_syscall_actor"));
+pub const INTEGER_OVERFLOW_ACTOR_BINARY: &[u8] =
+    include_bytes!(wasm_bin!("fil_integer_overflow_actor"));
+pub const SYSCALL_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_syscall_actor"));
+pub const EVENTS_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_events_actor"));
+pub const EXIT_DATA_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_exit_data_actor"));
+pub const GASLIMIT_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_gaslimit_actor"));
+pub const CREATE_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_create_actor"));
+pub const OOM_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_oom_actor"));
+pub const SSELF_ACTOR_BINARY: &[u8] = include_bytes!(wasm_bin!("fil_sself_actor"));
