42367553f53f7b0cdf1a55c7205d60d22f8399cb
zhiqiangxu <652732310@qq.com>
2023-06-30 11:23:16 +0800

    fix naming

--- shared/src/sector/registered_proof.rs
+++ shared/src/sector/registered_proof.rs
@@ -164,7 +164,7 @@ impl RegisteredPoStProof {
     }
     /// Returns the partition size, in sectors, associated with a proof type.
     /// The partition size is the number of sectors proven in a single PoSt proof.
-    pub fn window_post_partitions_sector(self) -> Result<u64, String> {
+    pub fn window_post_partition_sectors(self) -> Result<u64, String> {
         // Resolve to post proof and then compute size from that.
         use RegisteredPoStProof::*;
         match self {
@@ -200,7 +200,7 @@ impl RegisteredSealProof {
 
     /// Returns the partition size, in sectors, associated with a proof type.
     /// The partition size is the number of sectors proven in a single PoSt proof.
-    pub fn window_post_partitions_sector(self) -> Result<u64, String> {
+    pub fn window_post_partition_sectors(self) -> Result<u64, String> {
         // Resolve to seal proof and then compute size from that.
         use RegisteredSealProof::*;
         match self {
