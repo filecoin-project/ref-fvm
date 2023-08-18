7a3d17c16a5cdc560901564c1cf4c4f700454f49
Aayush Rajasekaran <arajasek94@gmail.com>
2023-08-17 21:31:33 -0400

    feat: support nv21 behind feature flag (#1846)

--- fvm/src/machine/default.rs
+++ fvm/src/machine/default.rs
@@ -59,8 +59,13 @@ where
     /// * `blockstore`: The underlying [blockstore][`Blockstore`] for reading/writing state.
     /// * `externs`: Client-provided ["external"][`Externs`] methods for accessing chain state.
     pub fn new(context: &MachineContext, blockstore: B, externs: E) -> anyhow::Result<Self> {
+        #[cfg(not(feature = "nv21-dev"))]
         const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
             NetworkVersion::V18..=NetworkVersion::V20;
+
+        #[cfg(feature = "nv21-dev")]
+        const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
+            NetworkVersion::V18..=NetworkVersion::V21;
 
         debug!(
             "initializing a new machine, epoch={}, base_fee={}, nv={:?}, root={}",
