844d5a9d29917920f75060e239120b9f6fad6d52
hanabi1224 <harlowmoo@gmail.com>
2023-09-27 15:12:19 +0800

    fix: publicly export GasDuration

--- fvm/src/gas/mod.rs
+++ fvm/src/gas/mod.rs
@@ -12,7 +12,7 @@ use num_traits::Zero;
 pub use self::charge::GasCharge;
 pub(crate) use self::outputs::GasOutputs;
 pub use self::price_list::{price_list_by_network_version, PriceList, WasmGasPrices};
-pub use self::timer::{GasInstant, GasTimer};
+pub use self::timer::{GasDuration, GasInstant, GasTimer};
 use crate::kernel::{ClassifyResult, ExecutionError, Result};
 
 mod charge;
