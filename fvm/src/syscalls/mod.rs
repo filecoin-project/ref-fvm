84a47d13c15dda853ea56f7fc1e9d4d6810d63b1
Steven Allen <steven@stebalien.com>
2023-10-26 12:15:00 -0700

    disable the actor-upgrade feature by default
    This is a minimal version of #1906 that still compiles the relevent
    code, just disables it at runtime.

--- fvm/src/syscalls/mod.rs
+++ fvm/src/syscalls/mod.rs
@@ -269,7 +269,11 @@ pub fn bind_syscalls(
     linker.bind("actor", "get_actor_code_cid", actor::get_actor_code_cid)?;
     linker.bind("actor", "next_actor_address", actor::next_actor_address)?;
     linker.bind("actor", "create_actor", actor::create_actor)?;
-    linker.bind("actor", "upgrade_actor", actor::upgrade_actor)?;
+    if cfg!(feature = "upgrade-actor") {
+        // We disable/enable with the feature, but we always compile this code to ensure we don't
+        // accidentally break it.
+        linker.bind("actor", "upgrade_actor", actor::upgrade_actor)?;
+    }
     linker.bind(
         "actor",
         "get_builtin_actor_type",
