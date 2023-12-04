e5ce4b3fa2c462296fdbf329f2bf8854990b3f4e
TippyFlits <james.bluett@protocol.ai>
2023-12-04 20:45:53 +0000

    Update fvm/src/gas/price_list.rs
    Co-authored-by: Aayush Rajasekaran <arajasek94@gmail.com>

--- fvm/src/gas/price_list.rs
+++ fvm/src/gas/price_list.rs
@@ -973,7 +973,9 @@ impl PriceList {
 /// Returns gas price list by NetworkVersion for gas consumption.
 pub fn price_list_by_network_version(network_version: NetworkVersion) -> &'static PriceList {
     match network_version {
-        NetworkVersion::V21 | NetworkVersion::V22 => &WATERMELON_PRICES,
+        NetworkVersion::V21 => &WATERMELON_PRICES,
+                #[cfg(feature = "nv21-dev")]
+        _ if network_version == NetworkVersion::V21 => &WATERMELON_PRICES,
         _ => panic!("network version {nv} not supported", nv = network_version),
     }
 }
