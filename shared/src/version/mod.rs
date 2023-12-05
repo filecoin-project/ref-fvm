aa2aa06b8737a22cf7518564b64c43b8e0d99b57
TippyFlits <james.bluett@protocol.ai>
2023-12-05 16:30:29 +0100

    feat: Add nv22 skeleton (#1929)
    * [WIP] feat: Add nv22 skeleton
    
    Addition of Network Version 22 skeleton
    
    * Update mod.rs
    
    * Update default.rs
    
    * Update mod.rs
    
    * Update fvm/src/gas/price_list.rs
    
    Co-authored-by: Aayush Rajasekaran <arajasek94@gmail.com>
    
    * Update nv21-dev to nv22-dev
    
    * Add nv22-dev to Cargo.toml
    
    * Fix formatting issue
    
    * Update fvm/src/gas/price_list.rs
    
    ---------
    
    Co-authored-by: Aayush Rajasekaran <arajasek94@gmail.com>

--- shared/src/version/mod.rs
+++ shared/src/version/mod.rs
@@ -57,6 +57,8 @@ impl NetworkVersion {
     pub const V20: Self = Self(20);
     /// Watermelon (builtin-actors v12)
     pub const V21: Self = Self(21);
+    /// TBD builtin-actors v13
+    pub const V22: Self = Self(22);
 
     pub const MAX: Self = Self(u32::MAX);
 
