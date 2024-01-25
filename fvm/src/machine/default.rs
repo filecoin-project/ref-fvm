9179b453c5a374aa10ffb2be4337367b2e7cd175
Steven Allen <steven@stebalien.com>
2024-01-24 21:08:40 -0800

    feat: enable nv22 support (#1970)
    We should have done this in v4.1, but I guess we're going straight for
    v4.1.1.

--- fvm/src/machine/default.rs
+++ fvm/src/machine/default.rs
@@ -58,11 +58,6 @@ where
     /// * `blockstore`: The underlying [blockstore][`Blockstore`] for reading/writing state.
     /// * `externs`: Client-provided ["external"][`Externs`] methods for accessing chain state.
     pub fn new(context: &MachineContext, blockstore: B, externs: E) -> anyhow::Result<Self> {
-        #[cfg(not(feature = "nv22-dev"))]
-        const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
-            NetworkVersion::V21..=NetworkVersion::V21;
-
-        #[cfg(feature = "nv22-dev")]
         const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
             NetworkVersion::V21..=NetworkVersion::V22;
 
