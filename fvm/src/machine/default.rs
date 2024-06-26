4f411192d25bf007c341bd78b52c5d49a4c4cd8e
Hubert <lesny.rumcajs+github@gmail.com>
2024-06-26 17:38:31 +0200

    chore: remove `nv23-dev` flag (#2022)

--- fvm/src/machine/default.rs
+++ fvm/src/machine/default.rs
@@ -51,11 +51,6 @@ where
     /// * `blockstore`: The underlying [blockstore][`Blockstore`] for reading/writing state.
     /// * `externs`: Client-provided ["external"][`Externs`] methods for accessing chain state.
     pub fn new(context: &MachineContext, blockstore: B, externs: E) -> anyhow::Result<Self> {
-        #[cfg(not(feature = "nv23-dev"))]
-        const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
-            NetworkVersion::V21..=NetworkVersion::V22;
-
-        #[cfg(feature = "nv23-dev")]
         const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
             NetworkVersion::V21..=NetworkVersion::V23;
 
