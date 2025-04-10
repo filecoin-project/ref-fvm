885f1220d21cc7e9cf2274be2bb11f13d1528010
Steven Allen <steven@stebalien.com>
2025-04-09 17:30:49 -0700

    chore: update deps

--- fvm/src/kernel/error.rs
+++ fvm/src/kernel/error.rs
@@ -1,7 +1,8 @@
 // Copyright 2021-2023 Protocol Labs
 // SPDX-License-Identifier: Apache-2.0, MIT
-use derive_more::Display;
+use derive_more::Display; // macro
 use fvm_shared::error::ErrorNumber;
+use std::fmt::Display; // trait
 
 /// Execution result.
 pub type Result<T> = std::result::Result<T, ExecutionError>;
