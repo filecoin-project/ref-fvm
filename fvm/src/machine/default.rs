e205dcf33a7c35ac38c80b02fab18566e2524584
TippyFlits <admin@yablu.com>
2025-04-28 20:06:32 +0100

    Apply rustfmt

--- fvm/src/machine/default.rs
+++ fvm/src/machine/default.rs
@@ -51,13 +51,13 @@ where
     /// * `blockstore`: The underlying [blockstore][`Blockstore`] for reading/writing state.
     /// * `externs`: Client-provided ["external"][`Externs`] methods for accessing chain state.
     pub fn new(context: &MachineContext, blockstore: B, externs: E) -> anyhow::Result<Self> {
-            #[cfg(not(feature = "nv27-dev"))]    
-            const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
-               NetworkVersion::V21..=NetworkVersion::V26;
-            
-	    #[cfg(feature = "nv27-dev")]
-	    const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
-	        NetworkVersion::V21..=NetworkVersion::V27;            
+        #[cfg(not(feature = "nv27-dev"))]
+        const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
+            NetworkVersion::V21..=NetworkVersion::V26;
+
+        #[cfg(feature = "nv27-dev")]
+        const SUPPORTED_VERSIONS: RangeInclusive<NetworkVersion> =
+            NetworkVersion::V21..=NetworkVersion::V27;
 
         debug!(
             "initializing a new machine, epoch={}, base_fee={}, nv={:?}, root={}",
