b6465fa829c1baa4a97dccecf8e12359bc654dab
Rod Vagg <rod@vagg.org>
2025-07-09 15:30:49 +1000

    chore: migrate conformance test runner to tokio (v4) (#2181)
    Switch from async-std to tokio runtime and maintain rough test execution time
    while simplifying the async implementation.
    
    Ref: https://github.com/filecoin-project/ref-fvm/pull/2173
    
    ---------
    
    Co-authored-by: Steven Allen <steven@stebalien.com>

--- testing/conformance/tests/runner.rs
+++ testing/conformance/tests/runner.rs
@@ -5,15 +5,13 @@ use std::collections::HashMap;
 use std::env::var;
 use std::fs::File;
 use std::io::BufReader;
-use std::iter;
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
-use std::thread::available_parallelism;
+use std::sync::Arc;
 
 use anyhow::{Context as _, anyhow};
-use async_std::{stream, sync, task};
 use colored::*;
-use futures::{Future, StreamExt, TryFutureExt, TryStreamExt};
+use futures::{StreamExt, stream};
 use fvm::engine::MultiEngine;
 use fvm_conformance_tests::driver::*;
 use fvm_conformance_tests::report;
@@ -48,7 +46,9 @@ lazy_static! {
         .map(|s| {
             let s = s.to_str().unwrap();
             s.parse().expect("parallelism must be an integer")
-        }).or_else(|| available_parallelism().map(Into::into).ok()).unwrap_or(8).min(48);
+        })
+        .unwrap_or_else(num_cpus::get)
+        .min(48);
 
     /// By default a post-condition error is fatal and stops all testing. We can use this env var to relax that
     /// and let the test carry on (optionally with a warning); there's a correctness check against the post condition anyway.
@@ -56,12 +56,13 @@ lazy_static! {
         .map(|s| {
             let s = s.to_str().unwrap();
             s.parse().expect("unexpected post condition error action")
-        }).unwrap_or(ErrorAction::Warn);
+        })
+        .unwrap_or(ErrorAction::Warn);
 
     static ref ENGINES: MultiEngine = MultiEngine::new(*TEST_VECTOR_PARALLELISM as u32);
 }
 
-#[async_std::test]
+#[tokio::test(flavor = "multi_thread")]
 async fn conformance_test_runner() -> anyhow::Result<()> {
     env_logger::init();
 
@@ -74,75 +75,63 @@ async fn conformance_test_runner() -> anyhow::Result<()> {
         .ok()
         .map(|path| TestTraceExporter::new(Path::new(path.as_str()).to_path_buf()));
 
-    let vector_results = if path.is_file() {
-        let stats = stats.clone();
-        let tracer = tracer.clone();
-        either::Either::Left(
-            iter::once(async move {
-                let res = run_vector(path.clone(), stats, tracer)
-                    .await
-                    .with_context(|| format!("failed to run vector: {}", path.display()))?;
-                anyhow::Ok((path, res))
-            })
-            .map(futures::future::Either::Left),
-        )
+    // Collect test vector files
+    let vector_paths = if path.is_file() {
+        vec![path]
     } else {
-        either::Either::Right(
-            WalkDir::new(path)
-                .into_iter()
-                .filter_ok(is_runnable)
-                .map(|e| {
-                    let stats = stats.clone();
-                    let tracer = tracer.clone();
-                    async move {
-                        let path = e?.path().to_path_buf();
-                        let res = run_vector(path.clone(), stats, tracer)
-                            .await
-                            .with_context(|| format!("failed to run vector: {}", path.display()))?;
-                        Ok((path, res))
-                    }
-                })
-                .map(futures::future::Either::Right),
-        )
+        WalkDir::new(path)
+            .into_iter()
+            .filter_ok(is_runnable)
+            .map(|e| e.map(|de| de.path().to_path_buf()))
+            .collect::<Result<Vec<_>, _>>()?
     };
 
-    let mut results = Box::pin(
-        stream::from_iter(vector_results)
-            // Will _load_ up to 100 vectors at once in any order. We won't actually run the vectors in
-            // parallel (yet), but that shouldn't be too hard.
-            .map(|task| {
-                async move {
-                    let (path, jobs) = task.await?;
-                    Ok(stream::from_iter(jobs).map(move |job| {
-                        let path = path.clone();
-                        Ok(async move { anyhow::Ok((path, job.await?)) })
-                    }))
-                }
-                .try_flatten_stream()
-            })
-            .flatten()
-            .try_buffer_unordered(*TEST_VECTOR_PARALLELISM),
-    );
+    let stats = Arc::new(stats);
+    let tracer = Arc::new(tracer);
+
+    // Process all test vectors concurrently, limited by buffer_unordered
+    let results = stream::iter(vector_paths)
+        .map(|path| {
+            let stats = stats.clone();
+            let tracer = tracer.clone();
+
+            async move {
+                // Run the vector processing in a blocking task
+                tokio::task::spawn_blocking(move || run_vector(path, stats, tracer)).await?
+            }
+        })
+        .buffer_unordered(*TEST_VECTOR_PARALLELISM)
+        .collect::<Vec<_>>()
+        .await;
 
     let mut succeeded = 0;
     let mut failed = 0;
     let mut skipped = 0;
 
-    while let Some((path, res)) = results.next().await.transpose()? {
-        match res {
-            VariantResult::Ok { id } => {
-                report!("OK".on_green(), path.display(), id);
-                succeeded += 1;
+    // Process results
+    for result in results.into_iter() {
+        match result {
+            Ok((path, variant_results)) => {
+                for res in variant_results {
+                    match res {
+                        VariantResult::Ok { id } => {
+                            report!("OK".on_green(), path.display(), id);
+                            succeeded += 1;
+                        }
+                        VariantResult::Failed { reason, id } => {
+                            report!("FAIL".white().on_red(), path.display(), id, reason);
+                            failed += 1;
+                        }
+                        VariantResult::Skipped { reason, id } => {
+                            report!("SKIP".on_yellow(), path.display(), id, reason);
+                            skipped += 1;
+                        }
+                    }
+                }
             }
-            VariantResult::Failed { reason, id } => {
-                report!("FAIL".white().on_red(), path.display(), id);
-                println!("\t|> reason: {:#}", reason);
+            Err(e) => {
+                eprintln!("Error processing vector: {:#}", e);
                 failed += 1;
-            }
-            VariantResult::Skipped { reason, id } => {
-                report!("SKIP".on_yellow(), path.display(), id);
-                println!("\t|> reason: {}", reason);
-                skipped += 1;
             }
         }
     }
@@ -159,7 +148,7 @@ async fn conformance_test_runner() -> anyhow::Result<()> {
         .bold()
     );
 
-    if let Some(ref stats) = stats {
+    if let Some(stats) = stats.as_ref().as_ref() {
         let stats = stats.lock().unwrap();
         println!(
             "{}",
@@ -174,7 +163,7 @@ async fn conformance_test_runner() -> anyhow::Result<()> {
         );
     }
 
-    if let Some(ref tracer) = tracer {
+    if let Some(tracer) = tracer.as_ref() {
         tracer.export_tombstones()?;
     }
 
@@ -187,31 +176,15 @@ async fn conformance_test_runner() -> anyhow::Result<()> {
 
 /// Runs a single test vector and returns a list of VectorResults,
 /// one per variant.
-async fn run_vector(
+fn run_vector(
     path: PathBuf,
-    stats: TestStatsRef,
-    tracer: TestTraceExporterRef,
-) -> anyhow::Result<impl Iterator<Item = impl Future<Output = anyhow::Result<VariantResult>>>> {
+    stats: Arc<TestStatsRef>,
+    tracer: Arc<TestTraceExporterRef>,
+) -> anyhow::Result<(PathBuf, Vec<VariantResult>)> {
     let file = File::open(&path)?;
     let reader = BufReader::new(file);
 
-    // Test vectors have the form:
-    //
-    //     { "class": ..., rest... }
-    //
-    // Unfortunately:
-    // 1. That means we need to use serde's "flatten" and/or "tag" feature to decode them.
-    // 2. Serde's JSON library doesn't support arbitrary precision numbers when doing this.
-    // 3. The circulating supply doesn't fit in a u64, and f64 isn't precise enough.
-    //
-    // So we manually:
-    // 1. Decode into a map of `String` -> `raw data`.
-    // 2. Pull off the class.
-    // 3. Re-serialize.
-    // 4. Decode into the correct type.
-    //
-    // Upstream bug is https://github.com/serde-rs/serde/issues/1183 (or at least that looks like
-    // the most appropriate one out of all the related issues).
+    // Parse the test vector
     let mut vector: HashMap<String, Box<serde_json::value::RawValue>> =
         serde_json::from_reader(reader).context("failed to parse vector")?;
     let class_json = vector
@@ -227,75 +200,76 @@ async fn run_vector(
             let v: MessageVector =
                 serde_json::from_str(&vector_json).context("failed to parse message vector")?;
             let skip = !v.selector.as_ref().is_none_or(Selector::supported);
+
             if skip {
-                Ok(either::Either::Left(
-                    v.preconditions.variants.into_iter().map(|variant| {
-                        futures::future::Either::Left(async move {
-                            Ok(VariantResult::Skipped {
-                                id: variant.id,
-                                reason: "selector not supported".to_owned(),
-                            })
-                        })
-                    }),
-                ))
-            } else {
-                // First import the blockstore and do some sanity checks.
-                let (bs, imported_root) = v.seed_blockstore().await?;
-                if !imported_root.contains(&v.preconditions.state_tree.root_cid) {
-                    return Err(anyhow!(
-                        "imported roots ({}) do not contain precondition CID {}",
-                        imported_root.iter().join(", "),
-                        v.preconditions.state_tree.root_cid
-                    ));
-                }
-                if !imported_root.contains(&v.postconditions.state_tree.root_cid) {
-                    let msg = format!(
-                        "imported roots ({}) do not contain postcondition CID {}",
-                        imported_root.iter().join(", "),
-                        v.postconditions.state_tree.root_cid
-                    );
-
-                    match *TEST_VECTOR_POSTCONDITION_MISSING_ACTION {
-                        ErrorAction::Error => {
-                            return Err(anyhow!(msg));
-                        }
-                        ErrorAction::Warn => {
-                            eprintln!("WARN: {msg} in {}", path.display())
-                        }
-                        ErrorAction::Ignore => (),
-                    }
-                }
-
-                let v = sync::Arc::new(v);
-                Ok(either::Either::Right(
-                    (0..v.preconditions.variants.len()).map(move |i| {
-                        let v = v.clone();
-                        let bs = bs.clone();
-                        let path = path.clone();
-                        let variant_id = v.preconditions.variants[i].id.clone();
-                        let name = format!("{} | {}", path.display(), variant_id);
-                        let stats = stats.clone();
-                        let tracer = tracer.clone();
-                        futures::future::Either::Right(
-                            task::Builder::new()
-                                .name(name.clone())
-                                .spawn(async move {
-                                    run_variant(
-                                        bs,
-                                        &v,
-                                        &v.preconditions.variants[i],
-                                        &ENGINES,
-                                        true,
-                                        stats,
-                                        tracer.map(|t| t.export_fun(path, variant_id)),
-                                    )
-                                    .with_context(|| format!("failed to run {name}"))
-                                })
-                                .unwrap(),
-                        )
-                    }),
-                ))
+                let results = v
+                    .preconditions
+                    .variants
+                    .into_iter()
+                    .map(|variant| VariantResult::Skipped {
+                        id: variant.id,
+                        reason: "selector not supported".to_owned(),
+                    })
+                    .collect();
+                return Ok((path, results));
             }
+
+            // Import the blockstore and do sanity checks
+            let (bs, imported_root) = v.seed_blockstore()?;
+            anyhow::ensure!(
+                imported_root.contains(&v.preconditions.state_tree.root_cid),
+                "imported roots ({}) do not contain precondition CID {}",
+                imported_root.iter().join(", "),
+                v.preconditions.state_tree.root_cid
+            );
+            if !imported_root.contains(&v.postconditions.state_tree.root_cid) {
+                let msg = format!(
+                    "imported roots ({}) do not contain postcondition CID {}",
+                    imported_root.iter().join(", "),
+                    v.postconditions.state_tree.root_cid
+                );
+
+                match *TEST_VECTOR_POSTCONDITION_MISSING_ACTION {
+                    ErrorAction::Error => {
+                        anyhow::bail!(msg);
+                    }
+                    ErrorAction::Warn => {
+                        eprintln!("WARN: {msg} in {}", path.display())
+                    }
+                    ErrorAction::Ignore => (),
+                }
+            }
+
+            // Run all variants
+            let results = v
+                .preconditions
+                .variants
+                .iter()
+                .map(|variant| {
+                    let variant_id = variant.id.clone();
+                    let name = format!("{} | {}", path.display(), variant_id);
+
+                    run_variant(
+                        bs.clone(),
+                        &v,
+                        variant,
+                        &ENGINES,
+                        true,
+                        stats.as_ref().clone(),
+                        tracer
+                            .as_ref()
+                            .clone()
+                            .map(|t| t.export_fun(path.clone(), variant_id.clone())),
+                    )
+                    .with_context(|| format!("failed to run {name}"))
+                    .unwrap_or_else(|e| VariantResult::Failed {
+                        id: variant_id,
+                        reason: e,
+                    })
+                })
+                .collect();
+
+            Ok((path, results))
         }
         other => Err(anyhow!("unknown test vector class: {}", other)),
     }
