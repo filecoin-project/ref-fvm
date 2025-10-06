12c389b4f93e7328a308676da2fbbac63ffac7f4
Phi-rjan <orjan.roren@gmail.com>
2025-10-06 11:24:54 +0200

    Update shared/src/version/mod.rs
    Co-authored-by: Rod Vagg <rod@vagg.org>

--- shared/src/version/mod.rs
+++ shared/src/version/mod.rs
@@ -67,7 +67,7 @@ impl NetworkVersion {
     pub const V25: Self = Self(25);
     /// Tock (builtin-actors v16)
     pub const V26: Self = Self(26);
-    /// TBD (TBD builtin-actor v17)
+    /// GoldenWeek (builtin-actor v17)
     pub const V27: Self = Self(27);
     /// TBD (TBD builtin-actor v18)
     pub const V28: Self = Self(28);
