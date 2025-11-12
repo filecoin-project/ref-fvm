2d0bbc024d714ff4325960e351ea13bad94e7eaf
Mikers <snissn@gmail.com>
2025-11-12 08:22:27 -1000

    ci(license): add SPDX headers to new fvm/tests and fix script to handle non-master base

--- fvm/tests/delegated_call_mapping.rs
+++ fvm/tests/delegated_call_mapping.rs
@@ -1,3 +1,5 @@
+// Copyright 2021-2023 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 mod common;
 
 use common::{install_evm_contract_at, new_harness, set_ethaccount_with_delegate};
--- fvm/tests/evm_extcode_projection.rs
+++ fvm/tests/evm_extcode_projection.rs
@@ -1,3 +1,5 @@
+// Copyright 2021-2023 Protocol Labs
+// SPDX-License-Identifier: Apache-2.0, MIT
 mod common;
 
 use common::{new_harness, set_ethaccount_with_delegate};
